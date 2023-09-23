f780b1db6af6992a65a5feed4dd05ac9862f0d37
Beat Hagenlocher <bah@posteo.de>
2023-09-23 22:23:12 +0200

    Move About to the left

--- index.html.pm
+++ index.html.pm
@@ -10,6 +10,20 @@
 		(p ([class "mt-4 max-w-xs leading-normal"])
 			"I enjoy learning.")
 	)
+
+	(section ([id "about"] [class "mb-16 scroll-mt-16 lg:mb-0 lg:scroll-mt-0"] [aria-label "About me"])
+			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:px-1 lg:py-1 lg:top-auto lg:mx-auto lg:w-full"])
+			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px]"])
+				      "About"))
+			 (div 
+			 	  (p ([class "mb-4"])
+				     "Story, story")
+			 	  (p ([class "mb-4"])
+				     "Interests, interests")
+			 	  (p ([class "mb-4"])
+				     "Me, me.")
+				))
+
 	(ul ([class "ml-1 mt-8 flex items-center"])
 	  (li ([class "mr-5 text-sm"])
 	      (a ([class "block hover:text-zinc-200"] [href "https://github.com/haglobah"] [target "_blank"] [rel "noreferrer"])
@@ -23,22 +37,12 @@
 			      (path ([d "M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"])))))
 	)
   )
+
+
 (main ([class "pt-24 lg:w-1/2 lg:py-24"])
-	(section ([id "about"] [class "mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"] [aria-label "About me"])
-			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"])
-			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px] lg:sr-only"])
-				      "About"))
-			 (div 
-			 	  (p ([class "mb-4"])
-				     "Story, story")
-			 	  (p ([class "mb-4"])
-				     "Interests, interests")
-			 	  (p ([class "mb-4"])
-				     "Me, me.")
-				))
 	(section ([id "experience"] [class "mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"] [aria-label "Work Experience"])
-			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"])
-			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px] lg:sr-only"])
+			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:px-1 lg:top-auto lg:mx-auto lg:w-full"])
+			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px]"])
 				      "Experience"))
 			 (div 
 			 	  (ol ([class "group/list"])
@@ -48,8 +52,8 @@
 					  )
 				))
 	(section ([id "projects"] [class "mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"] [aria-label "Selected Projects"])
-			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"])
-			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px] lg:sr-only"])
+			 (div ([class "sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:px-1 lg:top-auto lg:mx-auto lg:w-full"])
+			 	  (h2 ([class "text-base font-medium text-zinc-200 transition duration-200 ease-in-out hover:text-[#dc755c] uppercase tracking-[2px]"])
 				      "Projects"))
 			 (div 
 			 	  (ol ([class "group/list"])
